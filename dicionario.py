6b4a9a393df44adaca39ff30df464a7bd93d57f3
Heinrich37 <belforthenrich2004@gmail.com>
2025-05-14 21:55:40 -0400

    atv14/05

---
+++ dicionario.py
@@ -0,0 +1,303 @@
+# tradutor = {}
+# tradutor ["pineapple"] = "abacaxi"
+# tradutor ["apple"] = "maça"
+# tradutor ["orange"] = "laranja"
+# print(type(tradutor))
+# print(tradutor["apple"])
+
+#===============================================
+# tradutor1 = {}
+# tradutor1 = {"pineapple":"abacaxi", "apple":"maça", "orange":"laranja"}
+# print(type(tradutor1))
+# print(tradutor1)
+
+#===================================================
+
+# tradutor2 = {"pineapple":"abacaxi", "apple":"maça", "orange":"laranja"}
+# print(type(tradutor))
+# print(tradutor["apple"])
+# print(tradutor["pineapple"])
+# print(tradutor["orange"])
+
+#=======================================================
+# tradutor2 = {"pineapple":"abacaxi", "apple":"maça", "orange":"laranja"}
+# print(type(tradutor))
+# print(tradutor)
+# del(tradutor["pineapple"])
+# print(tradutor)
+#============================================================
+
+
+# tradutor3 = {"pineapple":"abacaxi", "apple":"maça", "orange":"laranja"}
+# print(tradutor3.pop("banana", "fruta"))
+
+
+#=======================================================
+# tradutor4 = {"pineapple":"abacaxi", "apple":"maça", "orange":"laranja"}
+# print(tradutor4)
+# tradutor4.clear["apple"])
+# print(tradutor4)
+
+#==================================================================
+
+# tradutor5 = {"pineapple":"abacaxi", "apple":"maça", "orange":"laranja"}
+# print("pineapple" in tradutor5)
+
+#=======================================================================
+
+# tradutor6 = {"pineapple":"abacaxi", "apple":"maça", "orange":"laranja"}
+# print(tradutor6.values())
+
+#==========================================================================
+
+# tradutor7 = {"pineapple":"abacaxi", "apple":"maça", "orange":"laranja"}
+# print(tradutor7)
+# tradutor7["apple"] = "goiaba"
+# print(tradutor7)
+
+#===========================================================================
+
+# dados = {
+#     "crossfox": {"Km": 35000, "ano": 2005}, 
+#     "DS5":{"km": 17000, "ano": 2015},
+#     "Fusca":{"km": 130000, "ano": 1979},
+#     "Jetta":{"Km": 56000, "ano": 2011},
+#     "Passat":{"km":62000, "ano": 1999}
+# }
+
+# print(dados["Fusca"]["ano"])
+# print(dados.get("gol"))
+
+#=============================================================================
+
+tradutor = {
+    "the": "o, a, os, as",
+    "of": "de",
+    "to": "para",
+    "and": "e",
+    "a": "um, uma",
+    "in": "em, dentro",
+    "is": "é",
+    "it": "isto",
+    "you": "você",
+    "that": "que, aquele",
+    "he": "ele",
+    "was": "era, estava",
+    "for": "por, para",
+    "on": "em",
+    "are": "são, estão",
+    "with": "com",
+    "as": "como, enquanto",
+    "I": "eu",
+    "his": "dele",
+    "they": "eles",
+    "be": "ser, estar",
+    "at": "em",
+    "one": "um",
+    "have": "ter",
+    "this": "este",
+    "from": "de",
+    "or": "ou",
+    "had": "tinha",
+    "by": "por",
+    "not": "não",
+    "word": "palavra",
+    "but": "mas",
+    "what": "o que",
+    "some": "alguns",
+    "we": "nós",
+    "can": "poder",
+    "out": "fora",
+    "other": "outro",
+    "were": "eram",
+    "all": "todos",
+    "there": "lá",
+    "when": "quando",
+    "up": "acima",
+    "use": "usar",
+    "your": "seu",
+    "how": "como",
+    "said": "disse",
+    "an": "um, uma",
+    "each": "cada",
+    "she": "ela",
+    "which": "qual",
+    "do": "fazer",
+    "their": "deles",
+    "time": "tempo",
+    "if": "se",
+    "will": "vai",
+    "way": "caminho",
+    "about": "sobre",
+    "many": "muitos",
+    "then": "então",
+    "them": "eles",
+    "write": "escrever",
+    "would": "iria",
+    "like": "gostar",
+    "so": "então",
+    "these": "estes",
+    "her": "dela",
+    "long": "longo",
+    "make": "fazer",
+    "thing": "coisa",
+    "see": "ver",
+    "him": "ele",
+    "two": "dois",
+    "has": "tem",
+    "look": "olhar",
+    "more": "mais",
+    "day": "dia",
+    "could": "poderia",
+    "go": "ir",
+    "come": "vir",
+    "did": "fez",
+    "number": "número",
+    "sound": "som",
+    "no": "não",
+    "most": "mais",
+    "people": "pessoas",
+    "my": "meu",
+    "over": "sobre",
+    "know": "saber",
+    "water": "água",
+    "than": "do que",
+    "call": "chamar",
+    "first": "primeiro",
+    "who": "quem",
+    "may": "pode",
+    "down": "baixo",
+    "side": "lado",
+    "been": "sido",
+    "now": "agora",
+    "find": "encontrar",
+    "any": "qualquer",
+    "new": "novo",
+    "work": "trabalho",
+    "part": "parte",
+    "take": "pegar",
+    "get": "obter",
+    "place": "lugar",
+    "made": "feito",
+    "live": "viver",
+    "where": "onde",
+    "after": "depois",
+    "back": "costas",
+    "little": "pouco",
+    "only": "apenas",
+    "round": "redondo",
+    "man": "homem",
+    "year": "ano",
+    "came": "veio",
+    "show": "mostrar",
+    "every": "todo",
+    "good": "bom",
+    "me": "me",
+    "give": "dar",
+    "our": "nosso",
+    "under": "sob",
+    "name": "nome",
+    "very": "muito",
+    "through": "através",
+    "just": "apenas",
+    "form": "forma",
+    "sentence": "frase",
+    "great": "grande",
+    "think": "pensar",
+    "say": "dizer",
+    "help": "ajuda",
+    "low": "baixo",
+    "line": "linha",
+    "differ": "diferir",
+    "turn": "virar",
+    "cause": "causa",
+    "much": "muito",
+    "mean": "significar",
+    "before": "antes",
+    "move": "mover",
+    "right": "direito",
+    "boy": "menino",
+    "old": "velho",
+    "too": "também",
+    "same": "mesmo",
+    "tell": "contar",
+    "does": "faz",
+    "set": "conjunto",
+    "three": "três",
+    "want": "querer",
+    "air": "ar",
+    "well": "bem",
+    "also": "também",
+    "play": "jogar",
+    "small": "pequeno",
+    "end": "fim",
+    "put": "colocar",
+    "home": "casa",
+    "read": "ler",
+    "hand": "mão",
+    "port": "porto",
+    "large": "grande",
+    "spell": "soletrar",
+    "add": "adicionar",
+    "even": "mesmo",
+    "land": "terra",
+    "here": "aqui",
+    "must": "deve",
+    "big": "grande",
+    "high": "alto",
+    "such": "tal",
+    "follow": "seguir",
+    "act": "agir",
+    "why": "por que",
+    "ask": "perguntar",
+    "men": "homens",
+    "change": "mudar",
+    "went": "foi",
+    "light": "luz",
+    "kind": "tipo",
+    "off": "desligado",
+    "need": "precisar",
+    "house": "casa",
+    "picture": "imagem",
+    "try": "tentar",
+    "us": "nós",
+    "again": "novamente",
+    "animal": "animal",
+    "point": "ponto",
+    "mother": "mãe",
+    "world": "mundo",
+    "near": "perto",
+    "build": "construir",
+    "self": "si mesmo",
+    "earth": "terra",
+    "father": "pai"
+}
+    
+while True:
+    print("\n1 - Traduzir uma palavra")
+    print("2 - Adicionar nova palavra")
+    print("3 - Sair")
+    
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == "1":
+        palavra = input("Digite uma palavra em inglês: ").lower()
+        traducao = tradutor.get(palavra)
+        if traducao:
+            print(f"A tradução de '{palavra}' é: {traducao}")
+        else:
+            print("Palavra não encontrada no dicionário.")
+
+    elif opcao == "2":
+        nova_palavra = input("Digite a nova palavra em inglês: ").lower()
+        nova_traducao = input("Digite a tradução em português: ").lower()
+        tradutor[nova_palavra] = nova_traducao
+        print(f"'{nova_palavra}' adicionada ao dicionário com a tradução '{nova_traducao}'.")
+
+
+    elif opcao == "3":
+        print("Saindo do programa...")
+        break
+
+    else:
+        print("Opção inválida. Tente novamente.")
